3e1fd53764261ce3354c600b883bebfbf0d36baf
Michael LoCascio <michael.s.locascio@gmail.com>
2022-06-10 18:45:42 -0700

    Createreactapp cleanup

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./topbar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+        <div className="topLeft">L</div>
+        <div className="topCenter">C</div>
+        <div className="topRight">R</div>
+    </div>
+  )
+}
