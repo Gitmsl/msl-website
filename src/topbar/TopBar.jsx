6a61aff3f2b47d07d5365be8ccbc0c53de0f571c
Michael LoCascio <michael.s.locascio@gmail.com>
2022-06-12 22:27:04 -0700

    Social media symbols
    + font change, + home about and contact list items for topbar

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -4,8 +4,17 @@ import "./topbar.css"
 export default function TopBar() {
   return (
     <div className="top">
-        <div className="topLeft">L</div>
-        <div className="topCenter">C</div>
+        <div className="topLeft">
+          <i className="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-github-square"></i>
+        </div>
+        <div className="topCenter">
+          <ul className="topList">
+            <li className="topListItem">HOME</li>
+            <li className="topListItem">ABOUT</li>
+            <li className="topListItem">CONTACT</li>
+          </ul>
+        </div>
         <div className="topRight">R</div>
     </div>
   )
