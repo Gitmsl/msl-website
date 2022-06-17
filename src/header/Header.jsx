f52dce9958b901fdb267b36804634824d763fe8f
Michael LoCascio <michael.s.locascio@gmail.com>
2022-06-16 20:34:40 -0700

    Refining header
    Adjusted styling for header component, added image for profile, though not rendering with localhost

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,8 +4,9 @@ import './header.css'
 export default function Header() {
     return(
         <div className="head">
-            <h1>Michael LoCascio</h1>
-            <h3>Software Engineer</h3>
+            <img src="./images/michael.jpg" className="michaelPic" alt="Michael LoCascio in a black suit"/>
+            <h1 className="name">Michael LoCascio</h1>
+            <h3 className="title">Software Engineer</h3>
             <p className="headText"> This is the content that will appear below the topbar</p>
         </div>
     )
