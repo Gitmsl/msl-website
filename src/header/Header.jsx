659ae9eb49af3c4b24587ff5ffa7707c5e5c534e
Michael LoCascio <michael.s.locascio@gmail.com>
2022-06-24 17:22:56 -0700

    Monkeying around
    New header background image (not final) and placeholder personal icon

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,10 +4,9 @@ import './header.css'
 export default function Header() {
     return(
         <div className="head">
-            <img src="./images/michael.jpg" className="michaelPic" alt="Michael LoCascio in a black suit"/>
+            <img src= "https://images.unsplash.com/photo-1516636052745-e142aecffd0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" className="michaelPic" alt="Michael LoCascio in a black suit"/>
             <h1 className="name">Michael LoCascio</h1>
             <h3 className="title">Software Engineer</h3>
-            <p className="headText"> This is the content that will appear below the topbar</p>
         </div>
     )
 }
