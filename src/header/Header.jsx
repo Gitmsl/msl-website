4f46c5b581b6863ae1cc7c1c5ad4c7d7064a9d0e
Michael LoCascio <michael.s.locascio@gmail.com>
2022-06-28 16:53:42 -0700

    Profile image
    Profile image added, some styling changes

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './header.css'
+import imageSrc from '../images/michael.jpg'
 
 export default function Header() {
     return(
         <div className="head">
-            <img src= "https://images.unsplash.com/photo-1516636052745-e142aecffd0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" className="michaelPic" alt="Michael LoCascio in a black suit"/>
+            <img src={imageSrc} alt="Michael LoCascio" className="michaelPic"/>
             <h1 className="name">Michael LoCascio</h1>
             <h3 className="title">Software Engineer</h3>
         </div>
