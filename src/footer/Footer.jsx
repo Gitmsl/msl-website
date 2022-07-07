ccf17b4dcb047096041abaadec837510738050c9
Michael LoCascio <michael.s.locascio@gmail.com>
2022-07-06 17:33:48 -0700

    Footer Styling
    Fixed position of footer, added background color to distinguish from other sections, changed font and font size to be minimally intrusive

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -3,8 +3,9 @@ import './footer.css'
 
 export default function Footer() {
     return(
-        <div className='footer'>
-            <p>Created by Michael LoCascio, 2022</p>
+        <div className="footer">
+            <p className='footerText'>Built with React by Michael LoCascio, 2022</p>
+            <a></a>
         </div>
     )
 }
