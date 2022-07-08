00c970cf00f8b581d33a32accea2114764207ace
Michael LoCascio <michael.s.locascio@gmail.com>
2022-07-07 18:28:19 -0700

    Resume Button (1/2)
    Resume button added, basic appearance and placement, hover effect.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import TopBar from "./topbar/TopBar"
 import Header from "./header/Header"
 import About from "./about/About"
 import Footer from "./footer/Footer"
+import Resume from "./resume/Resume"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <TopBar />
       <Header />
       <About />
+      <Resume />
       <Footer />
     </>
   );
---
+++ src/resume/Resume.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./resume.css"
+
+export default function Resume() {
+    return(
+        <div className="resume">
+            <a href="" target="_blank" className="resumeButton">Check out my Resume</a>
+        </div>
+    )
+}
