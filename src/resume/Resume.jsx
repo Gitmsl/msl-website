ae9b800e3bf8c448ed34ed15b7dd1793196ba933
Michael LoCascio <michael.s.locascio@gmail.com>
2022-07-08 17:32:59 -0700

    Resume button (2/2)
    Button functional, links to pdf of my current resume. Made minor styling changes.

--- src/resume/Resume.jsx
+++ src/resume/Resume.jsx
@@ -4,7 +4,7 @@ import "./resume.css"
 export default function Resume() {
     return(
         <div className="resume">
-            <a href="" target="_blank" className="resumeButton">Check out my Resume</a>
+            <a href="https://drive.google.com/file/d/1s7JA_iYs3_nUji4qqR2_1jmfKuWFps1a/view?usp=sharing" target="_blank" rel="noreferrer" className="resumeButton">Resume</a>
         </div>
     )
 }
