276dd439c56a24c5e03204fe9bba95c6b72a1286
Michael LoCascio <michael.s.locascio@gmail.com>
2022-07-20 20:10:03 -0700

    About styling and content
    Reworded about section, modified spacing and sizes, and introduced media queries to about.css to allow for sizing changes depending on screen width

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -5,14 +5,13 @@ export default function About() {
     return(
         <div className='about'>
             <h1 className='aboutTitle'>About Me</h1>
-            <p className='aboutText'>   Michael graduated from the University of California, Riverside with
-             a degree in Political Science in 2018, with plans to take a year or two
-             off before returning to higher-ed to pursue a PhD with the goal of
-             becoming a college professor. A few years and a global pandemic later, 
-             Michael re-evaluated his life plans, and began his journey to become
-             a self-taught software developer in 2021. Now, nearly half-way through 
-             2022, Michael is excited at the prospect of joining a team of fellow
-              developers eager and ready to identify and solve problems big and small. </p>
+            <p className='aboutText'>Hi, I'm Mike! I'm a PoliSci grad-turned self-taught software 
+            engineer from Southern California. I have a strong passion for education and mentorship
+            fostered by my time spent both learning and teaching at the University of California, Riverside;
+            passions that have proven valuable over the course of my transition towards a career in tech.
+            I've always had a passion for technology, having been raised on videogames and the early 
+            years of social media and the internet. I'm excited to join a group of fellow 
+            developers eager learn, grow, and provide value to their team. </p>
         </div>
     )
 }
