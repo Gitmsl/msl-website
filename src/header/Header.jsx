e053ba36a7c6b9ea1fbc38aff690ed20cd824703
Michael LoCascio <michael.s.locascio@gmail.com>
2022-10-18 14:48:20 -0700

    Header content update
    Header updated to be an integrated introduction, photo and about section side by side. Implemented CSS grid for ease of content placement.

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,9 +5,26 @@ import imageSrc from '../images/michael.jpg'
 export default function Header() {
     return(
         <div className="head">
-            <img src={imageSrc} alt="Michael LoCascio" className="michaelPic"/>
-            <h1 className="name">Michael LoCascio</h1>
-            <h3 className="title">Software Engineer</h3>
+            <div className="aboutItem">
+                <h1 className="name">Hi, I'm Mike!</h1>
+                <p className="title">
+                    Front-end software developer, team-player,
+                     communicator, excited to meet, work, and 
+                    learn with an enthusiastic group of 
+                    developers!
+                </p>
+                <p className="title">
+                    I’m currently open to full-time remote or 
+                    hybrid Software Engineer positions; If you’re
+                    interested in finding out more about me, I’d 
+                    love to connect!
+                </p>
+            </div>
+            <div className="profileItem">
+                <img src={imageSrc} alt="Michael LoCascio" className="michaelPic"/>
+                <h1 className="name"></h1>
+                <h3 className="title"></h3>
+            </div>
         </div>
     )
 }
